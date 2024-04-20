acd9175562016c1310380f1666e3f928b94bcbf3
Ruchira (Richie) Jayasekara <me@ruchij.com>
2024-04-20 19:53:50 +1000

    Implementing robot logic

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,48 @@
+import {isValidPosition, nextPosition, parseDirection, parseDirections} from "./index"
+import {BoardDimensions, Coordinate, Direction} from "./types"
 
-describe("This is a description", () => {
-    test("This is a test", () => {
-        const name: string = "Hello"
-        expect(name.toUpperCase()).toEqual("Hello")
+test("Should go to the expected next position", () => {
+    const position: Coordinate = {x: 0, y: 0}
+
+    expect(nextPosition(position, Direction.NORTH)).toStrictEqual({x: 0, y: 1})
+    expect(nextPosition(position, Direction.SOUTH)).toStrictEqual({x: 0, y: -1})
+    expect(nextPosition(position, Direction.WEST)).toStrictEqual({x: -1, y: 0})
+    expect(nextPosition(position, Direction.EAST)).toStrictEqual({x: 1, y: 0})
+})
+
+describe("Parsing direction", () => {
+    test("Should parse input string as Direction", () => {
+        expect(parseDirection("N")).toStrictEqual(Direction.NORTH)
+        expect(parseDirection("S")).toStrictEqual(Direction.SOUTH)
+        expect(parseDirection("W")).toStrictEqual(Direction.WEST)
+        expect(parseDirection("E")).toStrictEqual(Direction.EAST)
     })
+
+    test("Should return undefined for invalid direction string", () => {
+        expect(parseDirection("A")).not.toBeDefined()
+    })
+})
+
+describe("Parsing string of directions", () => {
+    test("Should parse string as an array of directions", () => {
+        expect(parseDirections("N E W S")).toStrictEqual([Direction.NORTH, Direction.EAST, Direction.WEST, Direction.SOUTH])
+    })
+
+    test("Should throw exception for an invalid directions", () => {
+        expect(() => parseDirections("N E W S B")).toThrow(new Error("Invalid direction: B"))
+    })
+})
+
+test("Should return whether the position is valid on the board", () => {
+    const boardDimensions: BoardDimensions = {height: 10, width: 10}
+
+    expect(isValidPosition(boardDimensions, {x: 1, y: 0})).toBeTruthy()
+    expect(isValidPosition(boardDimensions, {x: 0, y: 1})).toBeTruthy()
+    expect(isValidPosition(boardDimensions, {x: 9, y: 0})).toBeTruthy()
+    expect(isValidPosition(boardDimensions, {x: 0, y: 9})).toBeTruthy()
+
+    expect(isValidPosition(boardDimensions, {x: 0, y: 10})).toBeFalsy()
+    expect(isValidPosition(boardDimensions, {x: 10, y: 0})).toBeFalsy()
+    expect(isValidPosition(boardDimensions, {x: 0, y: -1})).toBeFalsy()
+    expect(isValidPosition(boardDimensions, {x: -1, y: 0})).toBeFalsy()
 })
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,63 @@
+import {BoardDimensions, Coordinate, Direction} from "./types"
 
-const sayHello = (): string => "Hello World"
+export const nextPosition =
+    (position: Coordinate, direction: Direction): Coordinate => {
+        const {x, y} = position
 
-console.log(sayHello())
+        switch (direction) {
+            case Direction.NORTH:
+                return {x, y: y + 1}
+            case Direction.EAST:
+                return {x: x + 1, y}
+            case Direction.SOUTH:
+                return {x, y: y - 1}
+            case Direction.WEST:
+                return {x: x - 1, y}
+        }
+    }
+
+export const isValidPosition =
+    (boardDimensions: BoardDimensions, position: Coordinate) =>
+        position.x >= 0 && position.y >= 0 && position.x < boardDimensions.width && position.y < boardDimensions.height
+
+export const parseDirections = (input: string): Direction[] => {
+    const directions: Direction[] =
+        input.split(" ")
+            .map(string => string.trim())
+            .filter(string => string.length > 0)
+            .map(string => {
+                const direction = parseDirection(string)
+
+                if (direction != undefined) {
+                    return direction
+                } else {
+                    throw new Error(`Invalid direction: ${string}`)
+                }
+            })
+
+    return directions
+}
+
+export const parseDirection = (input: string): Direction | undefined =>
+    Object.values(Direction).find(value => value === input)
+
+const move = (startingPosition: Coordinate, boardDimensions: BoardDimensions, directions: Direction[]): Coordinate =>
+    directions.reduce(
+        (position, direction) => {
+            const next: Coordinate = nextPosition(position, direction)
+
+            if (isValidPosition(boardDimensions, next)) {
+                return next
+            } else {
+                return position
+            }
+        },
+        startingPosition
+    )
+
+const start = (startingPosition: Coordinate, boardDimensions: BoardDimensions, inputDirections: string) => {
+    const directions: Direction[] = parseDirections(inputDirections)
+
+    const finalPosition = move(startingPosition, boardDimensions, directions)
+
+}
